4503e66af083ef5e2a919bd65835ffa0dc6af666
oldtree <treeui.old@gmail.com | 435594427@qq.com>
2015-01-05 15:09:46 +0800

    second day
    add a betch -process method to get more news before today

--- FlySpider.go
+++ FlySpider.go
@@ -7,22 +7,23 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"regexp"
+//	"regexp"
 	"strings"
 //	"bytes"
 	"encoding/json"
 	"strconv"
 	"flag"
+	"time"
 )
 
 
-var downloadPath = flag.String("-dpath",".","指定一个下载文件保存")
+var downloadPath = flag.String("-dpath","download/","指定一个下载文件保存")
 var targetUrl =flag.String("-durl","http://news-at.zhihu.com/api/3/news/latest","指定一个下载url")
-var DOWNLOAD = downloadPath
+var DOWNLOAD = *downloadPath
 
 
 var ZhihunewsUrl = "http://news-at.zhihu.com/api/3/news/latest"
-//var ZhihunewsUrl ="http://news.at.zhihu.com/api/3/news/before/20131119"
+var ZhihuBeforeNewsUrl ="http://news.at.zhihu.com/api/3/news/before/"
 var patch_shareUrl = "http://daily.zhihu.com/story/"
 
 //知乎新闻格式
@@ -50,15 +51,16 @@ type Item struct {
 
 
 
-func Creator(name string) {
-	file ,err :=os.Getwd()
-	sep :=strings.LastIndex(name,"/")
-	
-	err = os.Mkdir(name, 777)
-	
-	if err != nil {
-		panic(err)
+func Creator(name string)error {
+//	file ,err :=os.Getwd()
+	//sep :=strings.LastIndex(name,"/")
+	var err error
+	if isExist(name){
+		fmt.Println("file already exist")
+	}else{
+		err = os.Mkdir(DOWNLOAD+"/"+name, os.ModePerm)
 	}
+	return err
 }
 
 //将byte流转化为一个结构体
@@ -88,7 +90,7 @@ func isExist(path string)bool{
 }
 
 //根据新闻项的id和固定URL来取得新闻内容，并保存为.html文件
-func GetStoryContent(it Item){
+func GetStoryContent(it Item ,path string){
 	var content []byte
 	var filepath string
 	defer func(){
@@ -101,10 +103,10 @@ func GetStoryContent(it Item){
 	if err!=nil{
 		panic(err)
 	}
-	filepath = DOWNLOAD+it.Title+it.Theme_name+".html"
+	filepath = DOWNLOAD+path+"/"+it.Title+it.Theme_name+".html"
 	
 	content,err = ioutil.ReadAll(body.Body)
-    
+    fmt.Println(filepath)
 	if isExist(filepath){
 		fmt.Println("file already exist")
 	}else{
@@ -117,26 +119,32 @@ func GetStoryContent(it Item){
 	
 }
 
-//t := time.Date(2013, time.May, 20, 23, 0, 0, 0, time.UTC); t.Before(time.Now()); t = t.AddDate(0, 0, 1)
-func GetPageBody(urlpath string){
-	urlpath = ZhihunewsUrl
-	if urlpath=""{
+
+func GetPageBody(urlpath string,date string){
+	urlpath = urlpath
+	if urlpath==""{
 		fmt.Println("url path are null ")
 		return
 	}
-	resp, err := http.Get(urlpath)
 	defer func(){
 		v := recover()
 		if v!=nil{
 			fmt.Println(v)
 		}
 	}()
+	
+	Creator(date)
+	
+	resp, err := http.Get(urlpath)
+	defer resp.Body.Close()
+	
 	var content *TodayNews
 	var imagePath string
+	
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
+	
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -144,16 +152,18 @@ func GetPageBody(urlpath string){
 	}
 	
 	content = ParseContent([]byte(body))
-	fmt.Println(content.Date)
+
 	for _,item :=range  content.Story{
 		resp ,err :=http.Get(item.Images[0])
 		imgBody ,err:=ioutil.ReadAll(resp.Body)
 		if err!=nil{
 			panic(err)
 		}
-		GetStoryContent(item)
+		GetStoryContent(item,content.Date)
 		temp := strings.Split(item.Images[0],"/")
-		imagePath = DOWNLOAD+temp[len(temp)-1]
+		
+		imagePath = DOWNLOAD + date +"/"+temp[len(temp)-1]
+		fmt.Println(imagePath)
 		if isExist(imagePath){
 			fmt.Println("file already exist")
 		}else{
@@ -171,11 +181,109 @@ func GetPageBody(urlpath string){
 		if err!=nil{
 			panic(err)
 		}
+		fmt.Println(resp.Header)
 	}
 	return
 }
 
 
+func MonthToNumber(month string)string{
+	switch month{
+		case "January":
+		return "01"
+		case "February":
+		return "02"
+		case "March":
+		return "03"
+		case "April":
+		return "04"
+		case "May":
+		return "05"
+		case "June":
+		return "06"
+		case "July":
+		return "07"
+		case "August":
+		return "08"
+		case "September": 
+		return "09"
+		case "October":
+		return "10"
+		case "November":
+		return "11"
+		case "December":
+		return "12"
+	}
+	return "01"
+}
+
+
+func DayToNumber(day int)string{
+	switch day{
+		case 1:
+		return "01"
+		case 2:
+		return "02"
+		case 3:
+		return "03"
+		case 4:
+		return "04"
+		case 5:
+		return "05"
+		case 6:
+		return "06"
+		case 7:
+		return "07"
+		case 8:
+		return "08"
+		case 9: 
+		return "09"
+		default:
+		return strconv.Itoa(day)
+	}
+	return "01"
+}
+
+
+func GetDateString(t time.Time)string{
+	/*const layout = "2, 2006 at 3:04pm (MST)"
+	var ti time.Time
+	ti = time.Now()
+	//ti.Year()
+	ti.Format(layout)
+	fmt.Println(ti.Year())
+	fmt.Println(ti.Day())
+	fmt.Println(ti.Date())
+	*/
+	temp := strconv.Itoa(t.Year()) + MonthToNumber(t.Month().String()) + DayToNumber(t.Day())
+	return temp
+}
+
+func GetBeforeNews(start time.Time,end time.Time){
+	tCreate := time.Date(2013, time.May, 21, 12, 0, 0, 0, time.UTC)
+	
+	if start.Before(tCreate){
+		fmt.Println("start time nis before Zhihu's Dayly")
+		return
+	}
+	
+	if time.Now().Before(end){
+		fmt.Println("end time is faraway today")
+		return
+	}
+	
+	
+	for t := start; t.Before(end); t = t.AddDate(0, 0, 1){
+		date := GetDateString(t)
+		url := ZhihuBeforeNewsUrl + date
+		GetPageBody(url,date)
+	}
+}
+
 func main(){
-	GetPageBody("http://news-at.zhihu.com/api/3/news/latest")
+	//GetPageBody("http://news-at.zhihu.com/api/3/news/latest")
+	begin := time.Date(2014, time.December, 21, 18, 0, 0, 0, time.UTC)
+	
+	end := time.Date(2014, time.December, 25, 18, 0, 0, 0, time.UTC)
+	GetBeforeNews(begin,end)
 }
---
+++ Proxy.go
@@ -0,0 +1,170 @@
+package main
+
+/*
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+//	"os"
+	"regexp"
+	"strings"
+)
+
+// Cache templates
+var templates = template.Must(template.ParseFiles("home.html"))
+
+// Pre-compile RegEx
+var reBase = regexp.MustCompile("base +href=\"(.*?)\"")
+var reHTML = regexp.MustCompile("src=[\"\\'](.*?)[\"\\']|href=[\"\\'](.*?)[\"\\']|action=[\"\\'](.*?)[\"\\']")
+var reCSS = regexp.MustCompile("url\\([\"\\']?(.*?)[\"\\']?\\)")
+
+var httpClient *http.Client = &http.Client{}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// 404 for all other url path
+	if r.URL.Path[1:] != "" {
+		http.NotFound(w, r)
+		return
+	}
+	r.ParseForm()
+	enteredUrl := r.FormValue("url")
+	if enteredUrl != "" {
+		validUrl, _ := url.Parse(enteredUrl)
+		// prepend http if not specified
+		if validUrl.Scheme == "" {
+			validUrl.Scheme = "http"
+		}
+		encodedUrl := base64.StdEncoding.EncodeToString([]byte(validUrl.String()))
+		http.Redirect(w, r, "/p/"+encodedUrl, 302)
+		return
+	}
+	templates.ExecuteTemplate(w, "home.html", nil)
+}
+
+func encodeURL(src []byte, baseHref string, urlString string, start int, end int) []byte {
+	relURL := string(src[start:end])
+	// keep anchor and javascript links intact
+	if strings.Index(relURL, "#") == 0 || strings.Index(relURL, "javascript") == 0 {
+		return src
+	}
+	// Check if url is relative and make it absolute
+	if strings.Index(relURL, "http") != 0 {
+		var basePath *url.URL
+		if baseHref == "" {
+			basePath, _ = url.Parse(urlString)
+		} else {
+			basePath, _ = url.Parse(baseHref)
+		}
+		relPath, err := url.Parse(relURL)
+		if err != nil {
+			return src
+		}
+		absURL := basePath.ResolveReference(relPath).String()
+		src = bytes.Replace(src, []byte(relURL), []byte(absURL), -1)
+		end = start + len(absURL)
+	}
+	var encodedPath []byte = make([]byte, base64.StdEncoding.EncodedLen(end-start))
+	base64.StdEncoding.Encode(encodedPath, src[start:end])
+	return bytes.Replace(src, src[start:end], encodedPath, -1)
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	encodedUrl := r.URL.Path[len("/p/"):]
+	url, err := base64.StdEncoding.DecodeString(encodedUrl)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	urlString := string(url[:])
+	req, _ := http.NewRequest(r.Method, urlString, r.Body)
+	req.Header.Set("Content-Type", r.Header.Get("Content-Type"))
+	// Set request user agent to that of user's
+	req.Header.Set("User-Agent", r.Header.Get("User-Agent"))
+	
+	resp, err := httpClient.Do(req)
+	
+	if err != nil {
+		fmt.Println("Error Fetching " + urlString)
+		http.NotFound(w, r)
+		return
+	}
+	defer resp.Body.Close()
+
+	contentType := resp.Header.Get("Content-Type")
+	w.Header().Set("Content-Type", contentType)
+
+	// Rewrite all urls
+	baseHref := ""
+	if strings.Contains(contentType, "text/html") {
+		body, _ := ioutil.ReadAll(resp.Body)
+		baseHrefMatch := reBase.FindSubmatch(body)
+		if len(baseHrefMatch) > 0 {
+			baseHref = string(baseHrefMatch[1][:])
+		}
+		encodedBody := reHTML.ReplaceAllFunc(body, func(s []byte) []byte {
+			parts := reHTML.FindSubmatchIndex(s)
+			if parts != nil {
+				// replace src attribute
+				srcIndex := parts[2:4]
+				if srcIndex[0] != -1 {
+					return encodeURL(s, baseHref, urlString, srcIndex[0], srcIndex[1])
+				}
+
+				// replace href attribute
+				hrefIndex := parts[4:6]
+				if hrefIndex[0] != -1 {
+					return encodeURL(s, baseHref, urlString, hrefIndex[0], hrefIndex[1])
+				}
+				
+				// replace form action attribute
+				actionIndex := parts[6:8]
+				if actionIndex[0] != -1 {
+					return encodeURL(s, baseHref, urlString, actionIndex[0], actionIndex[1])
+				}
+			}
+			return s
+		})
+		w.Write(encodedBody)
+	} else if strings.Contains(contentType, "text/css") {
+		body, _ := ioutil.ReadAll(resp.Body)
+		encodedBody := reCSS.ReplaceAllFunc(body, func(s []byte) []byte {
+			parts := reCSS.FindSubmatchIndex(s)
+			if parts != nil {
+				// replace url attribute in css
+				pathIndex := parts[2:4]
+				if pathIndex[0] != -1 {
+					return encodeURL(s, baseHref, urlString, pathIndex[0], pathIndex[1])
+				}
+			}
+			return s
+		})
+		w.Write(encodedBody)
+	} else {
+		io.Copy(w, resp.Body)
+	}
+}
+
+func Proxy_main() {
+	var httpHost string = "localhost"
+	var httpPort string = "8080"	
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/p/", proxyHandler)
+
+	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	fmt.Printf("web proxy listening on %s:%s\n", httpHost, httpPort)
+
+	http.ListenAndServe(httpHost+":"+httpPort, nil)
+
+}
+*/
